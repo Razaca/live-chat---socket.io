fc3926148d2e5ec6c0b430fbaf3b50f03078e21c
Renzo Cervantes <r33n84@gmail.com>
2023-09-11 18:30:02 -0300

    back changes

--- server/handlers/handlerMessages.js
+++ server/handlers/handlerMessages.js
@@ -2,7 +2,9 @@ export const handlerMessages = (io, socket, messages) => {
   socket.on("message", (message) => {
     messages[message.roomName].push(message.message);
 
-    socket.to(message.roomName);
+    socket
+      .to(message.roomName)
+      .emit("message", { messages: messages[message.roomName] });
     io.to(socket.id).emit("message", { messages: messages[message.roomName] });
   });
 };
