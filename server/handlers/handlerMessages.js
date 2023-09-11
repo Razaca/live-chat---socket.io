19bc796b999f19bee482cbcf96434c3b820e43b2
Renzo Cervantes <r33n84@gmail.com>
2023-09-11 18:22:57 -0300

    fix error in send messages to a room

--- server/handlers/handlerMessages.js
+++ server/handlers/handlerMessages.js
@@ -2,9 +2,7 @@ export const handlerMessages = (io, socket, messages) => {
   socket.on("message", (message) => {
     messages[message.roomName].push(message.message);
 
-    socket
-      .to(message.roomName)
-      .emit("message", { messages: messages[message.roomName] });
+    socket.to(message.roomName);
     io.to(socket.id).emit("message", { messages: messages[message.roomName] });
   });
 };
