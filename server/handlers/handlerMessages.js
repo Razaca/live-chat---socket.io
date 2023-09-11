b47ec49032613ff4c5d0ba23166934d8a9937b0e
Renzo Cervantes <r33n84@gmail.com>
2023-09-11 00:47:12 -0300

    fix error in message

--- server/handlers/handlerMessages.js
+++ server/handlers/handlerMessages.js
@@ -5,6 +5,6 @@ export const handlerMessages = (io, socket, messages) => {
     socket
       .to(message.roomName)
       .emit("message", { messages: messages[message.roomName] });
-    socket.emit("message", { messages: messages[message.roomName] });
+    io.to(socket.id).emit("message", { messages: messages[message.roomName] });
   });
 };
